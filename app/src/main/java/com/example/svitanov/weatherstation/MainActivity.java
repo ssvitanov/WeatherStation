7837b5d01c9c852752c2f5852a31e0863e7421bc
ssvitanov <ssvitanov@gmail.com>
2019-04-07 18:31:58 +0300

    added BluetoothSPP library

--- app/src/main/java/com/example/svitanov/weatherstation/MainActivity.java
+++ app/src/main/java/com/example/svitanov/weatherstation/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.svitanov.weatherstation;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Set;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+        }
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+
+        if (pairedDevices.size() > 0) {
+            // There are paired devices. Get the name and address of each paired device.
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC address
+
+                if (deviceName.equals("HC-06"))
+                {
+//                    ConnectThread connectThread = new ConnectThread(device, getApplicationContext());
+//                    connectThread.start();
+                }
+            }
+        }
     }
 }
